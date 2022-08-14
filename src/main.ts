99ad0d3832044bb5a8e9c87185260361b02eaa71
Yi-Ru Lan <a78945612385238@gmail.com>
2022-08-15 04:40:49 +0800

    Adding Inheritance & Generics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-// Project Status (Enum)
+// Project Status Enum
 enum ProjectStatus {
   Active,
   Finished,
 }
 
-// Project Class (Type)
+// Project Type
 class Project {
   constructor(
     public id: string,
@@ -15,25 +15,45 @@ class Project {
   ) {}
 }
 
-// Project State Management
-type Listener = (items: Project[]) => void;
+interface Role<T> {
+  name: T;
+}
 
-class ProjectState {
-  private listeners: Listener[] = [];
+// Project State Management
+type Listener<T> = (items: T[]) => void; // 泛型也可用於 type 或 interface (概念同樣為藉由外部傳遞類型來定義類型)
+
+class State<T> {
+  /**
+   * 泛型傳遞: 與一般泛型傳遞沒有差別 (這邊設計為由 State 接受泛型並轉發給 Listener 泛型，兩邊使用相同泛型標識符就如同不同方法使用相同參數名稱本就不會有任何影響)
+   * protected 修飾元: 相比 private 修飾元將允許子類訪問 (但仍然無法由外部訪問)
+   */
+  protected listeners: Listener<T>[] = [];
+
+  public addListener(listenerFunc: Listener<T>) {
+    this.listeners.push(listenerFunc);
+  }
+}
+
+class ProjectState extends State<Project> {
   private projects: Project[] = []; // 陣列的每個項目為 Project 的實例 (class 相較 interface 除同樣可用來約束物件外，還可實例化物件)
   private static instance: ProjectState;
 
-  private constructor() {}
-
-  public static getInstance() {
-    if (this.instance) return this.instance;
-
-    this.instance = new ProjectState();
-    return this.instance;
+  /**
+   * 私有建構函式: 通常用於單例模式下確保建構函式無法由外部實例化，僅類內部可實例化 (由類靜態方法選擇返回新實例或已存在實例)
+   */
+  private constructor() {
+    super();
   }
 
-  public addListener(listenerFunc: Listener) {
-    this.listeners.push(listenerFunc);
+  public static getInstance() {
+    /**
+     * 全域實例: 為可在任何地方使用的實例常數 (通常類定義完後就馬上實例化，即 new ProjectState()，參考下面調用 getInstance 方法位置)
+     * 單例模式: 為確保整個應用都使用同個實例的設計手段 (保證類始終只有一個實例即每次的調用都將返回同個實例)
+     * 使用目的: 本就只想提供唯一的全域狀態管理物件 (調用相同實例可使狀態始終同步)
+     */
+    if (!this.instance) this.instance = new ProjectState();
+
+    return this.instance;
   }
 
   public addProject(title: string, description: string, numOfPeople: number) {
@@ -58,18 +78,12 @@ class ProjectState {
   }
 }
 
-/**
- * 全域實例: 為可在任何地方使用的實例常數 (通常類定義完後就馬上實例化)
- * 單例模式: 為確保整個應用都使用同個實例的設計手段 (保證類始終只有一個實例即每次的調用都將返回同個實例)
- * 使用目的: 本就只想提供唯一的全域狀態管理物件 (調用相同實例可使狀態始終同步)
- */
-// const projectState = new ProjectState();
 const projectState = ProjectState.getInstance();
 
-// Validation
+// Validation Utility
 interface Validatable {
   value: string | number;
-  required?: boolean; // 也可寫為 required: boolean | undefined (就為問號的實際表示)
+  required?: boolean; // 也可寫為 required: boolean | undefined (就為問號的實際表示)(函式參數也為同個概念)
   minLength?: number;
   maxLength?: number;
   min?: number;
@@ -114,7 +128,7 @@ function validate(validatableInput: Validatable) {
   return true;
 }
 
-// Autobind Decorator (接受函式陳述式或函式表達式)
+// Autobind Function Decorator
 function Autobind(_: any, __: string, descriptor: PropertyDescriptor) {
   return {
     configurable: true,
@@ -124,20 +138,50 @@ function Autobind(_: any, __: string, descriptor: PropertyDescriptor) {
   };
 }
 
+// Component Base Class
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement; // 當元素不存在特定類型時可分配 HTMLElement 類型 (為所有特定元素類型的基礎通用類型)
+  hostElement: T; // 透過泛型來分配動態類型使元素類型可根據子類傳入類型而定義 (約束為 HTMLElement 使泛型仍可獲得元素通用屬性提示)
+  element: U;
+
+  constructor(
+    templateElementId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string,
+  ) {
+    this.templateElement = document.getElementById(templateElementId) as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId) as T;
+    this.element = document.importNode(this.templateElement.content, true).firstElementChild as U; // content 為 HTMLTemplateElement 中的屬性，將提供模板內容的引用
+
+    if (newElementId) this.element.id = newElementId;
+
+    this.attach(insertAtStart);
+  }
+
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeginning ? 'afterbegin' : 'beforeend',
+      this.element,
+    );
+  }
+
+  /**
+   * 抽象類: 使類無法被直接實例化 (應始終僅用於繼承)
+   * 抽象方法: 僅定義類型而不存在實現 (子類得存在該方法且根據所定義外觀類型進行實作)
+   * 誤區: 抽象類仍舊可包含已實作的方法，僅標記為抽象的方法需單純定義類型 (並不是抽象類中僅可存在抽象的對象。把抽象類、方法分開並單純從定義來看就對了)
+   */
+  public abstract configure(): void; // 也可添加問號將其轉為可選方法使子類不被強迫添加 (如 abstract configure?(): void)
+
+  public abstract renderContent(): void; // 抽象修飾元無法與私有修飾元並用即抽象修飾元始終與公共修飾元並用 (公共抽象修飾元使子類必須存在該方法且只能是公共的)
+}
+
 // ProjectList Class
-class ProjectList {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLElement; // 可透過 HTMLElement 表示不存在元素類型 (屬於其他特定元素類型的基類)
+class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
 
   constructor(private type: 'active' | 'finished') {
-    this.templateElement = document.querySelector('#project-list')!;
-    this.hostElement = document.querySelector('#app')!;
-
-    const importedNode = document.importNode(this.templateElement.content, true);
-    this.element = importedNode.firstElementChild as HTMLElement;
-    this.element.id = `${this.type}-projects`;
+    super('project-list', 'app', false, `${type}-projects`); // 調用 super 所傳遞參數同樣不能使用 this 引用
 
     /**
      * 初始化字段: 所在 addListener 的回調並不會在建構函式立刻被調用，故不算初始化字段
@@ -145,6 +189,11 @@ class ProjectList {
      */
     this.assignedProjects = [];
 
+    this.configure();
+    this.renderContent();
+  }
+
+  public configure() {
     /**
      * 註冊偵聽器函式: 傳遞回調給 projectState 進行管理，也表示回調將由 projectState 主動調用才被觸發
      * 觸發偵聽器回調: 由調用 projectState.addProject 方法間接調用回調 (同時接收已更新的項目列表)
@@ -152,16 +201,16 @@ class ProjectList {
      */
     projectState.addListener((projects: Project[]) => {
       const relevantProjects = projects.filter(({ status }) => {
-        return this.type === 'active'
-          ? status === ProjectStatus.Active
-          : status === ProjectStatus.Finished;
+        return status === (this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
       });
       this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
+  }
 
-    this.attach();
-    this.renderContent();
+  public renderContent() {
+    this.element.querySelector('ul')!.id = `${this.type}-projects-list`;
+    this.element.querySelector('h2')!.textContent = `${this.type.toUpperCase()} PROJECTS`;
   }
 
   private renderProjects() {
@@ -173,28 +222,17 @@ class ProjectList {
       return listItemElement;
     });
 
-    // 通常會使用 innerHTML 先清除舊有的所有內容後再進行渲染 (可確保舊有內容不會跟著被渲染)
-    // listElement.innerHTML = '';
-
-    // 更快速的取代所有子元素方法 (除 IE 外的瀏覽器都已支援)
+    /**
+     * innerHTML: 一般較常使用 innerHTML 並將其設為空字串以清除元素底下的所有元素 (此指 listElement.innerHTML = '')
+     * replaceChildren: 除 IE 外的瀏覽器皆已支援的新快速清除/覆蓋方法 (不指定覆蓋對象將使其清除底下所有元素，指定覆蓋對象將使其覆蓋底下所有元素)
+     * 目的: 重新渲染的初始行為。意即先清除舊有內容再進行渲染 (確保舊有內容不會遺留於元素上)
+     */
     listElement.replaceChildren(...listItemElements);
-  }
-
-  private renderContent() {
-    this.element.querySelector('ul')!.id = `${this.type}-projects-list`;
-    this.element.querySelector('h2')!.textContent = `${this.type.toUpperCase()} PROJECTS`;
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement('beforeend', this.element);
   }
 }
 
 // ProjectInput Class
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
   peopleInputElement: HTMLInputElement;
@@ -204,27 +242,33 @@ class ProjectInput {
    * 單獨方法: 進行插入 (Insertion) 或微調 (Fine Tuning) 邏輯
    */
   constructor() {
+    super('project-input', 'app', true, 'user-input');
+
     /**
      * getElementById: 不支持泛型，必須使用類型轉換
      * querySelector: 支持泛型，僅指示為非 null 即可
      */
-    this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
-    this.hostElement = document.getElementById('app') as HTMLDivElement;
-
-    /**
-     * content: 為 HTMLTemplateElement 中的屬性，將提供模板內容的引用
-     */
-    const importedNode = document.importNode(this.templateElement.content, true);
-    this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = 'user-input';
-
     this.titleInputElement = this.element.querySelector('#title')!;
     this.descriptionInputElement = this.element.querySelector('#description')!;
     this.peopleInputElement = this.element.querySelector('#people')!;
 
+    /**
+     * 字段初始化行為必須存在於建構函式中 (意指無法透過調用存在初始化字段行為的方法來進行初始化，TypeScript 會認為依舊沒有初始化故報錯)
+     */
     this.configure();
-    this.attach();
   }
+
+  /**
+   * 常見約定為 public 方法在上 (建構函式下)，而 private 方法在下
+   */
+  public configure() {
+    /**
+     * 偵聽器的回調將使 this 指向偵聽元素，可透過手動 bind 或裝飾器自動 bind 來解決問題
+     */
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  public renderContent() {}
 
   private gatherUserInput(): [string, string, number] | void {
     /**
@@ -268,8 +312,12 @@ class ProjectInput {
     this.peopleInputElement.value = '';
   }
 
+  /**
+   * Event: 為所有特定行為預設分配的特定事件的基礎通用類型
+   * SubmitEvent: 為 submit 行為預設分配的特定事件
+   */
   @Autobind
-  private submitHandler(e: Event) {
+  private submitHandler(e: SubmitEvent) {
     e.preventDefault();
 
     /**
@@ -289,17 +337,6 @@ class ProjectInput {
 
       this.clearInputs();
     }
-  }
-
-  private configure() {
-    /**
-     * 偵聽器的回調將使 this 指向偵聽元素，可透過手動 bind 或裝飾器自動 bind 來解決問題
-     */
-    this.element.addEventListener('submit', this.submitHandler);
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
 
